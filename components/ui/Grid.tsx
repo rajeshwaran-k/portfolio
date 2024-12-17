fa0cd900741382e318ceab3c5c71fd63b6c22665
Rajeshwaran <110971058+rajeshwaran-k@users.noreply.github.com>
2024-12-17 22:14:23 +0530

    added assests and BentoGrid on progress

--- components/ui/Grid.tsx
+++ components/ui/Grid.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import { BentoGrid } from "./BentoGrid";
+import { BentoGrid, BentoGridItem } from "./BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid>{}</BentoGrid>
+      <BentoGrid>
+        {gridItems.map((i) => (
+          <BentoGridItem
+            id={i.id}
+            key={i.key}
+            title={i.title}
+            description={i.description}
+          />
+        ))}
+      </BentoGrid>
     </section>
   );
 };
