1de1612e8d103400bd8d429bbbfd7e0508d38c21
Rajeshwaran <110971058+rajeshwaran-k@users.noreply.github.com>
2024-12-19 19:54:46 +0530

    finish bentoGrid component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import Grid from "@/components/ui/Grid";
-import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
 export default function Home() {
