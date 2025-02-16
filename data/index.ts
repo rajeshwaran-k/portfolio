c886f3e2f5f0f780426aa1a33040a507f93411c4
Rajeshwaran <110971058+rajeshwaran-k@users.noreply.github.com>
2025-02-16 20:54:33 +0530

    sync

--- data/index.ts
+++ data/index.ts
@@ -1,212 +1,217 @@
-
 export const navItems = [
-    { name: "About", link: "#about" },
-    { name: "Projects", link: "#projects" },
-    { name: "Contact", link: "#contact" },
-    {name: "Experience", link:"#workExperience"}
-  ];
-  
-  export const gridItems = [
-    {
-      id: 1,
-      title: "Turning Ideas into real life products is my calling",
-      description: "",
-      className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
-      imgClassName: "w-full h-full",
-      titleClassName: "justify-end",
-      img: "/b1.svg",
-      spareImg: "",
-    },
-    {
-      id: 2,
-      title: "I'm very flexible with time zone communications",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 3,
-      title: "My tech stack",
-      description: "I constantly try to improve",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-2",
-      imgClassName: "",
-      titleClassName: "justify-center",
-      img: "",
-      spareImg: "",
-    },
-    {
-      id: 4,
-      title: "Tech enthusiast with a passion for development.",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-start",
-      img: "/grid.svg",
-      spareImg: "/b4.svg",
-    },
-  
-    {
-      id: 5,
-      title: "Currently building a collaborative quiz app",
-      description: "The Inside Scoop",
-      className: "md:col-span-3 md:row-span-2",
-      imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
-      titleClassName: "justify-center md:justify-start lg:justify-center",
-      img: "/b5.svg",
-      spareImg: "/grid.svg",
-    },
-    {
-      id: 6,
-      title: "Interested in hiring me ?",
-      description: "",
-      className: "lg:col-span-2 md:col-span-3 md:row-span-1",
-      imgClassName: "",
-      titleClassName: "justify-center md:max-w-full max-w-60 text-center",
-      img: "",
-      spareImg: "",
-    },
-  ];
-  
-  export const projects = [
-    {
-      id: 1,
-      title: "Custom CRM platform",
-      des: "Custom CRM platform built from scratch with tighter integration with client's other internal systems.",
-      img: "/skl.png",
-      iconLists: [ "/next.svg",  "/nestJs.svg", "/ts.svg", "/pg.svg", "/aws.svg"],
-      link: "https://cybermindworks.com/portfolio/crm",
-      linkName: "More info"
-    },
-    {
-      id: 2,
-      title: "LMS platform",
-      des: "Learning Management System with interactive course delivery, real-time assessments, and personalized learning paths.",
-      img: "/grow.png",
-      iconLists: [ "/next.svg",  "/nestJs.svg", "/ts.svg", "/pg.svg", "/aws.svg"],
-      link: "https://grow.skill-lync.com/",
-      linkName: "Check Live Site"
-    },
-    {
-      id: 3,
-      title: "Live Demo Platform",
-      des: "A platform built to schedule instant live demo with customers",
-      img: "/matchMaking.png",
-      iconLists: [ "/ts.svg", "/pg.svg", "/next.svg",  "/nestJs.svg", "/aws.svg", "/hostinger.svg"],
-      link: "https://cybermindworks.com/portfolio/demo-portal",
-      linkName: "More info"
-    },
-    {
-      id: 4,
-      title: "Online Test Platform",
-      des: "End to end test platform which includes MCQ, Coding and Audio/Video Questions",
-      img: "/blackbuck2.png",
-      iconLists: ["/next.svg",  "/nestJs.svg",  "/ts.svg", "/pg.svg", "/aws.svg"],
-      link: "https://taptap.blackbucks.me/",
-      linkName: "Check Live Site"
-    },
-  ];
-  
-  export const testimonials = [
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-    {
-      quote:
-        "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-      name: "Michael Johnson",
-      title: "Director of AlphaStream Technologies",
-    },
-  ];
-  
-  export const companies = [
-    {
-      id: 1,
-      name: "cloudinary",
-      img: "/cloud.svg",
-      nameImg: "/cloudName.svg",
-    },
-    {
-      id: 2,
-      name: "appwrite",
-      img: "/app.svg",
-      nameImg: "/appName.svg",
-    },
-    {
-      id: 3,
-      name: "HOSTINGER",
-      img: "/host.svg",
-      nameImg: "/hostName.svg",
-    },
-    {
-      id: 4,
-      name: "stream",
-      img: "/s.svg",
-      nameImg: "/streamName.svg",
-    },
-    {
-      id: 5,
-      name: "docker.",
-      img: "/dock.svg",
-      nameImg: "/dockerName.svg",
-    },
-  ];
-  
-  export const workExperience = [
-    {
-      id: 1,
-      title: "Full Stack Web Developer @CybermindWorks",
-      desc: "Developed and maintained multiple Full stack web applications for multiple clients using latest technologies.",
-      className: "md:col-span-2",
-      thumbnail: "/exp4.svg",
-    },
-    {
-      id: 2,
-      title: "Freelance Web Developer",
-      desc: "Worked with a team of three in multiple freelance projects",
-      className: "md:col-span-2",
-      thumbnail: "/exp3.svg",
-    }
-  ];
-  
-  export const socialMedia = [
-    {
-      id: 1,
-      img: "/git.svg",
-      link:"https://github.com/rajeshwaran-k"
-    },
-    {
-      id: 2,
-      img: "/whatsapp.svg",
-      link:"https://wa.me/919361482922"
-    },
-    {
-      id:3,
-      img: "/link.svg",
-      link:"https://www.linkedin.com/in/rajeshwaran-k-b8a4021ba/"
-    }
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+  { name: "Experience", link: "#workExperience" },
+];
 
-  ];
+export const gridItems = [
+  {
+    id: 1,
+    title: "Turning Ideas into real life products is my calling",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a collaborative quiz app",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Interested in hiring me?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Custom CRM platform",
+    des: "Custom CRM platform built from scratch with tighter integration with client's other internal systems.",
+    img: "/skl.png",
+    iconLists: ["/next.svg", "/nestJs.svg", "/ts.svg", "/pg.svg", "/aws.svg"],
+    link: "https://cybermindworks.com/portfolio/crm",
+    linkName: "More info",
+  },
+  {
+    id: 2,
+    title: "LMS platform",
+    des: "Learning Management System with interactive course delivery, real-time assessments, and personalized learning paths.",
+    img: "/grow.png",
+    iconLists: ["/next.svg", "/nestJs.svg", "/ts.svg", "/pg.svg", "/aws.svg"],
+    link: "https://grow.skill-lync.com/",
+    linkName: "Check Live Site",
+  },
+  {
+    id: 3,
+    title: "Live Demo Platform",
+    des: "A platform built to schedule instant live demo with customers",
+    img: "/matchMaking.png",
+    iconLists: [
+      "/ts.svg",
+      "/pg.svg",
+      "/next.svg",
+      "/nestJs.svg",
+      "/aws.svg",
+      "/hostinger.svg",
+    ],
+    link: "https://cybermindworks.com/portfolio/demo-portal",
+    linkName: "More info",
+  },
+  {
+    id: 4,
+    title: "Online Test Platform",
+    des: "End to end test platform which includes MCQ, Coding and Audio/Video Questions",
+    img: "/blackbuck2.png",
+    iconLists: ["/next.svg", "/nestJs.svg", "/ts.svg", "/pg.svg", "/aws.svg"],
+    link: "https://taptap.blackbucks.me/",
+    linkName: "Check Live Site",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+  {
+    quote:
+      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+    name: "Michael Johnson",
+    title: "Director of AlphaStream Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Web Developer @CybermindWorks",
+    desc: "Developed and maintained multiple Full stack web applications for multiple clients using latest technologies.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+  {
+    id: 2,
+    title: "Freelance Web Developer",
+    desc: "Worked with a team of three in multiple freelance projects",
+    className: "md:col-span-2",
+    thumbnail: "/exp3.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/rajeshwaran-k",
+  },
+  {
+    id: 2,
+    img: "/whatsapp.svg",
+    link: "https://wa.me/919361482922",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/rajeshwaran-k-b8a4021ba/",
+  },
+];
